42a865a6a25cb3f9a795164245505064918553dd
Juan Guana <juanc.guana@gmail.com>
2021-11-23 20:08:46 -0500

    feat: :sparkles: Example with express

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('Hola mundo');
+const express = require('express');
+const routerApi = require('./routes');
+const cors = require('cors');
+const {
+  logErrors,
+  errorHandler,
+  boomeErrorHandler,
+} = require('./middlewares/error.handle');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const whiteList = ['http://127.0.0.1:5500', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomeErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const productsRouter = require('./products.router');
+const usersRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+
+function routerApi(app) {
+  const router = express.Router()
+  app.use('/api/v1', router)
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+}
+
+module.exports = routerApi;
